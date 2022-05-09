57df4c661b0453a8594d79a278f0e03807853085
Michael Spilotro <michaelspilotro13@gmail.com>
2022-05-09 17:48:29 -0500

    python learning

---
+++ Test/test.py
@@ -0,0 +1,19 @@
+
+character_name = "john"
+character_age = "35"
+print("howdy " + character_name + ", ")
+print("age " +character_age)
+
+
+print("/___|")
+
+phrase = "Cool stuff"
+print(phrase + " is cool")
+print(phrase.lower().islower())
+print("Giraffe\"Academy")
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("C"))
+print(phrase.replace("stuff", "Sponge"))
+
+

dd7338cd6ee2c37718afbcdf7ae99d2e35643c8d
Michael Spilotro <michaelspilotro13@gmail.com>
2022-05-09 19:20:45 -0500

    3

---
+++ Test/Tuples.py
@@ -0,0 +1,6 @@
+print("container to store different values, kinda like lists but a little different")
+coordinates = (4, 5)
+print("can't modify tuple once created")
+print(coordinates[0])
+
+Coordinates_cool = [(4, 9), (6, 7)]
--- Test/test.py
+++ Test/test.py
@@ -17,3 +17,42 @@ print(phrase.index("C"))
 print(phrase.replace("stuff", "Sponge"))
 
 
+
+from math import *
+
+print("Numbers")
+print(3 + 4.5)
+print("remainder of 10/3")
+print(10 % 3)
+my_num = -5
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 8))
+print(round(3.3))
+print(floor(3.7))
+print(ceil(3.9))
+print(sqrt(36))
+
+print("input from user")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+
+print("Basic Calculator")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+
+print(result)
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+

182e8fd0514fbbad5f72d725262adcb7d14d4683
Michael Spilotro <michaelspilotro13@gmail.com>
2022-05-11 20:29:56 -0500

    4

--- Test/Dictionary.py
+++ Test/Dictionary.py
@@ -2,8 +2,9 @@ monthConversions = {
     "Jan": "January",
     "Feb": "February",
     "Mar": "March",
-    "Apr": "April",
+    1: "April",
 }
 
 print(monthConversions["Jan"])
+print(monthConversions.get("cool", "Not a valid key"))
 
